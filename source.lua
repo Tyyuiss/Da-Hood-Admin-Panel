48367b1452634de590c86c99e6731335de4d02cf
Tyyuiss <73793955+Tyyuiss@users.noreply.github.com>
2022-07-31 19:33:28 -0500

    Update source.lua
    Say !changelog in game to see the changes!

--- source.lua
+++ source.lua
@@ -5,7 +5,6 @@ local MainFrame = Instance.new("Frame")
 local FindPlayerServerIDTB = Instance.new("TextBox")
 local PlayerNameTB = Instance.new("TextBox")
 local JoinServer = Instance.new("TextButton")
-local BlanKSPACE = Instance.new("TextLabel")
 local PERMA = Instance.new("TextButton")
 local UNBAN = Instance.new("TextButton")
 local UserID = Instance.new("TextBox")
@@ -15,6 +14,14 @@ local Format = Instance.new("TextButton")
 local Spectate = Instance.new("TextButton")
 local UnBan = Instance.new("TextButton")
 local BanUnbanNoteHere = Instance.new("TextBox")
+local BlankSpace = Instance.new("TextLabel")
+local ChangeLog = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
 
 --Properties:
 
@@ -27,13 +34,13 @@ MainFrame.Name = "MainFrame"
 MainFrame.Parent = DHPanel
 MainFrame.BackgroundColor3 = Color3.fromRGB(180, 182, 184)
 MainFrame.BorderSizePixel = 2
-MainFrame.Position = UDim2.new(0.656316757, 0, 0.513655305, 0)
+MainFrame.Position = UDim2.new(0.678740978, 0, 0.458802998, 0)
 MainFrame.Size = UDim2.new(0.306824207, 0, 0.437656581, 0)
 
 FindPlayerServerIDTB.Name = "FindPlayerServerIDTB"
 FindPlayerServerIDTB.Parent = MainFrame
 FindPlayerServerIDTB.BackgroundColor3 = Color3.fromRGB(183, 254, 108)
-FindPlayerServerIDTB.Size = UDim2.new(0.497841269, 0, 0.0895420089, 0)
+FindPlayerServerIDTB.Size = UDim2.new(0.5, 0, 0.0900000036, 0)
 FindPlayerServerIDTB.Font = Enum.Font.SourceSans
 FindPlayerServerIDTB.PlaceholderColor3 = Color3.fromRGB(0, 0, 0)
 FindPlayerServerIDTB.PlaceholderText = "Find Player Server ID"
@@ -61,7 +68,7 @@ JoinServer.Name = "JoinServer"
 JoinServer.Parent = MainFrame
 JoinServer.BackgroundColor3 = Color3.fromRGB(131, 141, 204)
 JoinServer.Position = UDim2.new(0, 0, 0.090000011, 0)
-JoinServer.Size = UDim2.new(0.498449147, 0, 0.0894590914, 0)
+JoinServer.Size = UDim2.new(0.5, 0, 0.0890000015, 0)
 JoinServer.Font = Enum.Font.SourceSans
 JoinServer.Text = "Join Server"
 JoinServer.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -69,23 +76,11 @@ JoinServer.TextScaled = true
 JoinServer.TextSize = 14.000
 JoinServer.TextWrapped = true
 
-BlanKSPACE.Name = "BlanKSPACE"
-BlanKSPACE.Parent = MainFrame
-BlanKSPACE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-BlanKSPACE.Position = UDim2.new(0.500000238, 0, 0.090000011, 0)
-BlanKSPACE.Size = UDim2.new(0.499999732, 0, 0.0894590914, 0)
-BlanKSPACE.Font = Enum.Font.SourceSans
-BlanKSPACE.Text = ""
-BlanKSPACE.TextColor3 = Color3.fromRGB(0, 0, 0)
-BlanKSPACE.TextScaled = true
-BlanKSPACE.TextSize = 14.000
-BlanKSPACE.TextWrapped = true
-
 PERMA.Name = "PERMA"
 PERMA.Parent = MainFrame
 PERMA.BackgroundColor3 = Color3.fromRGB(226, 112, 97)
 PERMA.Position = UDim2.new(0, 0, 0.181999996, 0)
-PERMA.Size = UDim2.new(0.497841239, 0, 0.0464983061, 0)
+PERMA.Size = UDim2.new(0.5, 0, 0.0460000001, 0)
 PERMA.Font = Enum.Font.SourceSans
 PERMA.Text = "PERMA"
 PERMA.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -97,7 +92,7 @@ UNBAN.Name = "UNBAN"
 UNBAN.Parent = MainFrame
 UNBAN.BackgroundColor3 = Color3.fromRGB(218, 189, 175)
 UNBAN.Position = UDim2.new(0, 0, 0.230000004, 0)
-UNBAN.Size = UDim2.new(0.497841179, 0, 0.0460000001, 0)
+UNBAN.Size = UDim2.new(0.5, 0, 0.0460000001, 0)
 UNBAN.Font = Enum.Font.SourceSans
 UNBAN.Text = "UNBAN"
 UNBAN.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -123,7 +118,7 @@ UserID2.Name = "UserID2"
 UserID2.Parent = MainFrame
 UserID2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 UserID2.Position = UDim2.new(0.5, 0, 0.230000004, 0)
-UserID2.Size = UDim2.new(0.499999911, 0, 0.0459999964, 0)
+UserID2.Size = UDim2.new(0.49999994, 0, 0.0459999964, 0)
 UserID2.Font = Enum.Font.SourceSans
 UserID2.PlaceholderColor3 = Color3.fromRGB(38, 38, 38)
 UserID2.PlaceholderText = "UserID"
@@ -145,9 +140,9 @@ PlayerList.ScrollBarThickness = 10
 Format.Name = "Format"
 Format.Parent = PlayerList
 Format.BackgroundColor3 = Color3.fromRGB(224, 74, 44)
-Format.Position = UDim2.new(0, 0, -0.00209336984, 0)
-Format.Size = UDim2.new(0.244855598, 0, 0.0199999996, 0)
-Format.Font = Enum.Font.Ubuntu
+Format.Position = UDim2.new(0, 0, -0.000165187172, 0)
+Format.Size = UDim2.new(0.245000005, 0, 0.0199999996, 0)
+Format.Font = Enum.Font.TitilliumWeb
 Format.Text = "Username"
 Format.TextColor3 = Color3.fromRGB(0, 0, 0)
 Format.TextScaled = true
@@ -159,7 +154,7 @@ Spectate.Parent = Format
 Spectate.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Spectate.Position = UDim2.new(3.03412414, 0, -0.00199983316, 0)
 Spectate.Size = UDim2.new(1.0468061, 0, 1.01000178, 0)
-Spectate.Font = Enum.Font.Ubuntu
+Spectate.Font = Enum.Font.TitilliumWeb
 Spectate.Text = "RESET"
 Spectate.TextColor3 = Color3.fromRGB(0, 0, 0)
 Spectate.TextScaled = true
@@ -169,9 +164,9 @@ Spectate.TextWrapped = true
 UnBan.Name = "UnBan"
 UnBan.Parent = Format
 UnBan.BackgroundColor3 = Color3.fromRGB(52, 133, 90)
-UnBan.Position = UDim2.new(1, 0, -0.00199969718, 0)
-UnBan.Size = UDim2.new(1.04202127, 0, 1.01000214, 0)
-UnBan.Font = Enum.Font.Ubuntu
+UnBan.Position = UDim2.new(1.00000012, 0, -0.00199969718, 0)
+UnBan.Size = UDim2.new(1.04202127, 0, 1.0019995, 0)
+UnBan.Font = Enum.Font.TitilliumWeb
 UnBan.Text = "UnBan"
 UnBan.TextColor3 = Color3.fromRGB(0, 0, 0)
 UnBan.TextScaled = true
@@ -183,7 +178,7 @@ BanUnbanNoteHere.Parent = Format
 BanUnbanNoteHere.BackgroundColor3 = Color3.fromRGB(180, 182, 184)
 BanUnbanNoteHere.Position = UDim2.new(2.04202199, 0, -0.00199992745, 0)
 BanUnbanNoteHere.Size = UDim2.new(0.993405879, 0, 1.01000166, 0)
-BanUnbanNoteHere.Font = Enum.Font.Ubuntu
+BanUnbanNoteHere.Font = Enum.Font.Arial
 BanUnbanNoteHere.PlaceholderColor3 = Color3.fromRGB(0, 0, 0)
 BanUnbanNoteHere.PlaceholderText = "Ban/UnBan Note Here"
 BanUnbanNoteHere.Text = ""
@@ -192,9 +187,83 @@ BanUnbanNoteHere.TextScaled = true
 BanUnbanNoteHere.TextSize = 14.000
 BanUnbanNoteHere.TextWrapped = true
 
+BlankSpace.Name = "BlankSpace"
+BlankSpace.Parent = MainFrame
+BlankSpace.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BlankSpace.Position = UDim2.new(0.500000596, 0, 0.0900000036, 0)
+BlankSpace.Size = UDim2.new(0.499999523, 0, 0.0889999568, 0)
+BlankSpace.Font = Enum.Font.SourceSans
+BlankSpace.Text = ""
+BlankSpace.TextColor3 = Color3.fromRGB(0, 0, 0)
+BlankSpace.TextScaled = true
+BlankSpace.TextSize = 14.000
+BlankSpace.TextWrapped = true
+
+ChangeLog.Name = "ChangeLog"
+ChangeLog.Parent = DHPanel
+ChangeLog.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+ChangeLog.Position = UDim2.new(0.302424282, 0, 0.295358628, 0)
+ChangeLog.Size = UDim2.new(0.376363635, 0, 0.473914593, 0)
+ChangeLog.Visible = false
+
+UICorner.CornerRadius = UDim.new(0.100000001, 8)
+UICorner.Parent = ChangeLog
+
+TextLabel.Parent = ChangeLog
+TextLabel.BackgroundColor3 = Color3.fromRGB(97, 97, 97)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.0436135493, 0, 0.0940014645, 0)
+TextLabel.Size = UDim2.new(0.888566613, 0, 0.101937599, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "-Added Spectation Functionality"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+TextButton.Parent = ChangeLog
+TextButton.BackgroundColor3 = Color3.fromRGB(216, 33, 9)
+TextButton.Position = UDim2.new(0.186915889, 0, 0.83608979, 0)
+TextButton.Size = UDim2.new(0.598898351, 0, 0.0870987475, 0)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Close"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+UICorner_2.Parent = TextButton
+
+TextLabel_2.Parent = ChangeLog
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(97, 97, 97)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.0436135493, 0, 0.194905668, 0)
+TextLabel_2.Size = UDim2.new(0.888566613, 0, 0.101937599, 0)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "-Added Fake Bans | Unbans"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_3.Parent = ChangeLog
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(97, 97, 97)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.0436135493, 0, 0.414520681, 0)
+TextLabel_3.Size = UDim2.new(0.888566613, 0, 0.101937599, 0)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "More Coming Soon"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+
 -- Scripts:
 
-local function TAXITHT_fake_script() -- PlayerList.LocalScript 
+local function APTSHNR_fake_script() -- PlayerList.LocalScript 
 	local script = Instance.new('LocalScript', PlayerList)
 
 	local cam = workspace.CurrentCamera
@@ -271,16 +340,16 @@ local function TAXITHT_fake_script() -- PlayerList.LocalScript
 	    updatepanel(0)
 	end)
 end
-coroutine.wrap(TAXITHT_fake_script)()
-local function LVJR_fake_script() -- Format.LocalScript 
+coroutine.wrap(APTSHNR_fake_script)()
+local function XHAZG_fake_script() -- Format.LocalScript 
 	local script = Instance.new('LocalScript', Format)
 
 	script.Parent.Spectate.MouseButton1Click:Connect(function()
 	    workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
 	end)
 end
-coroutine.wrap(LVJR_fake_script)()
-local function HHYA_fake_script() -- MainFrame.Handler 
+coroutine.wrap(XHAZG_fake_script)()
+local function WUVLA_fake_script() -- MainFrame.Handler 
 	local script = Instance.new('LocalScript', MainFrame)
 
 	local UIS = game:GetService('UserInputService')
@@ -356,4 +425,22 @@ local function HHYA_fake_script() -- MainFrame.Handler
 	    Duration = 15
 	})
 end
-coroutine.wrap(HHYA_fake_script)()
+coroutine.wrap(WUVLA_fake_script)()
+local function ALPI_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+	    script.Parent.Parent.Visible = false
+	end)
+end
+coroutine.wrap(ALPI_fake_script)()
+local function TKEAC_fake_script() -- ChangeLog.Handler 
+	local script = Instance.new('LocalScript', ChangeLog)
+
+	game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	    if msg == '!changelog' then
+	        script.Parent.Visible = true
+	    end
+	end)
+end
+coroutine.wrap(TKEAC_fake_script)()
